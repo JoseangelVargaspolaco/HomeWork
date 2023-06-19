9fbd7e09054d8d49940f1ce0986c520cda51950a
JoseAngelVargasPolaco <jose_vargas7@ucne.edu.do>
2023-06-19 17:36:14 -0400

    commit inicial

--- Program.cs
+++ Program.cs
@@ -2,97 +2,87 @@ using System;
 
 namespace HomeWork
 {
-    class HomeWork
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Bucle principal para volver a ejecutar el programa
             do
             {
-                int N_Estudiantes = 0; 
-                int N_years = 0;
-                string Nombre_Carrera = "Ingenieria en sistemas"; 
-                int T_estudents = 0;
+                int N_Estudiantes;
+                int N_years;
+                int aulas;
+                string Nombre_Carrera = "Ingenieria en sistemas";
 
-                // Solicitar al usuario el número de estudiantes
                 Console.Write("\n- Ingrese el número de estudiantes: ");
                 N_Estudiantes = Convert.ToInt32(Console.ReadLine());
 
-                // Solicitar al usuario el número de años de la carrera
-                Console.Write("- Ingrese el número de años de la carrera: ");
+                Console.Write("\n- Ingrese el número de años: ");
                 N_years = Convert.ToInt32(Console.ReadLine());
 
-                // Validar que el número de años esté dentro del rango válido (1-5)
-                while (N_years < 1 || N_years > 5)
-                {
-                    Console.Clear();
-                    Console.WriteLine("\n- El número de años debe estar entre 1-5.");
-                    Console.Write("- Ingrese el número de años de la carrera (1-5): ");
-                    N_years = Convert.ToInt32(Console.ReadLine());
-                }
+                Console.WriteLine($"- Nombre de la carrera: {Nombre_Carrera}");
 
-                // Solicitar al usuario el nombre de la carrera
-                Console.Write($"- Nombre de la carrera: {Nombre_Carrera}");
+                int[] pasaron = new int[N_years];
+                int[] quemados = new int[N_years];
+                int[] cojieron_miedo = new int[N_years];
 
-                //Probabilidad de que los estudiantes que pasen, se quemen o de asusten desde el primer año hasta el quinto año
-                double[] pasaron = { 0.7, 0.7, 0.7, 0.7, 0.7 };
-                double[] quemados = { 0.2, 0.2, 0.2, 0.2, 0.2 }; 
-                double[] cojieron_miedo = { 0.1, 0.1, 0.1, 0.1, 0.1 }; 
+                int N_EstudiantesInicial = N_Estudiantes;
 
-                int[] M_inscrita = new int[N_years]; // Matrículas inscritas por año
-                int[] E_aprobados = new int[N_years]; // Aprobados por año
-                int[] E_reprobados = new int[N_years]; // Reprobados por año
-                int[] E_abandonaron = new int[N_years]; // Abandonos por año
-
-                // Ciclo para simular los años de la carrera
                 for (int i = 0; i < N_years; i++)
                 {
-                    M_inscrita[i] = N_Estudiantes;
-                    E_aprobados[i] = (int)(N_Estudiantes * pasaron[i]); // Cálculo del número de estudiantes aprobados en el año i
-                    E_abandonaron[i] = (int)(N_Estudiantes * quemados[i]); // Cálculo del número de estudiantes que abandonan en el año i
-                    E_reprobados[i] = (int)(N_Estudiantes * cojieron_miedo[i]); // Cálculo del número de estudiantes reprobados en el año i
-                    
-                    N_Estudiantes = E_aprobados[i] + E_reprobados[i]; // Número de estudiantes que pasan al siguiente año
+                    Random random = new Random();
+
+                    pasaron[i] = random.Next(70, 101);
+                    quemados[i] = random.Next(60, 69);
+                    cojieron_miedo[i] = random.Next(0, 10);
                 }
 
-                // Cálculo del número de aulas requeridas
-                for (int y = 0; y < N_years; y++)
+                int[] M_inscrita = new int[N_years * 3];
+                int[] E_aprobados = new int[N_years * 3];
+                int[] E_reprobados = new int[N_years * 3];
+                int[] E_abandonaron = new int[N_years * 3];
+
+                for (int año = 1; año <= N_years; año++)
                 {
-                    T_estudents += M_inscrita[y];
+                    for (int semestre = 1; semestre <= 3; semestre++)
+                    {
+                        int indice = (año - 1) * 3 + semestre - 1;
+
+                        M_inscrita[indice] = N_Estudiantes;
+
+                        Random random = new Random();
+
+                        E_aprobados[indice] = (int)(N_Estudiantes * (pasaron[año - 1] / 100.0));
+                        E_reprobados[indice] = (int)(N_Estudiantes * (quemados[año - 1] / 100.0));
+                        E_abandonaron[indice] = (int)(N_Estudiantes * (cojieron_miedo[año - 1] / 100.0));
+
+                        N_Estudiantes -= E_abandonaron[indice];
+
+                        aulas = (int)Math.Ceiling((double)N_Estudiantes / 30);
+
+                        Console.WriteLine("\n ------ Año {0} - Semestre {1} ------", año, semestre);
+                        Console.WriteLine(" \n\t- Matriculados: {0} - \n\t - Aprobados: {1} - \n\t - Repitentes: {2} - \n\t - Abandonaron: {3} - \n\t - Aulas disponibles: {4} -",
+                            M_inscrita[indice], E_aprobados[indice], E_reprobados[indice], E_abandonaron[indice], aulas);
+                    }
                 }
 
-                int aulas = (int)Math.Ceiling((double)T_estudents / 30); // Considerando una capacidad de 30 estudiantes por aula
+                int ultimoAño = N_years;
+                int ultimoSemestre = 3;
+                int indiceUltimo = (ultimoAño - 1) * 3 + ultimoSemestre - 1;
+                int ultimoAulaInicial = (ultimoAño - 1) * 3 + 1;
 
-                // Imprimir resultados
-                Console.WriteLine("\nResultados:");
-                Console.WriteLine();
-                Console.WriteLine($"Carrera: {Nombre_Carrera}\n");
+                aulas = (int)Math.Ceiling((double)N_EstudiantesInicial / 30);
+                Console.WriteLine("\nTotal de aulas requeridas en el último semestre: {0}", aulas);
 
-                for (int e = 0; e < N_years; e++)
-                {
-                    Console.WriteLine($"Año {e + 1}:");
-                    Console.WriteLine($"- Matriculados: {M_inscrita[e]}"); // Imprimir el número de estudiantes matriculados en el año i
-                    Console.WriteLine($"- Aprobados: {E_aprobados[e]}"); // Imprimir el número de estudiantes aprobados en el año i
-                    Console.WriteLine($"- Reprobados: {E_reprobados[e]}"); // Imprimir el número de estudiantes reprobados en el año i
-                    Console.WriteLine($"- Abandonos: {E_abandonaron[e]}"); // Imprimir el número de estudiantes que abandonan en el año i
-                    Console.WriteLine();
-                }
-
-                Console.WriteLine($"Total de aulas requeridas: {aulas}"); // Imprimir el número total de aulas requeridas
-
-                // Validación para ejecutar de nuevo
-                Console.WriteLine();
-                Console.Write("Pulsa cualquier otra tecla para ejecutar de nuevo o si quieres salir ingresa 'S': ");
+                Console.Write("Pulsa cualquier otra tecla para ejecutar de nuevo o 'S' para salir: ");
                 string input = Console.ReadLine();
-                if (input.ToUpper() == "S" || input.ToUpper() == "s")
+                if (input.ToUpper() == "S")
                 {
-                    break; // Salir del bucle principal y finalizar el programa
+                    break;
                 }
 
-                Console.Clear(); // Limpiar la consola antes de ejecutar nuevamente
+                Console.Clear();
 
             } while (true);
         }
     }
 }
-
